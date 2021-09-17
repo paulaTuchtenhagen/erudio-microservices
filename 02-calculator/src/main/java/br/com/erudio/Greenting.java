4a5d0f2d90ffc4e0ce5b47248133baeb56671d15
Paula Carvalho <paula.carvalho@logicinfo.com>
2021-09-17 15:58:30 -0300

    initial commit

---
+++ 02-calculator/src/main/java/br/com/erudio/Greenting.java
@@ -0,0 +1,24 @@
+package br.com.erudio;
+
+public class Greenting {
+	
+	private final long id;
+	private final String content;
+	
+	public Greenting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+	
+	
+	 
+
+}
